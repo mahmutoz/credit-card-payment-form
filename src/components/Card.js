9dfae28d030c067b7573c1711dfce23452c76e04
mahmutoz <mahmutoz8197@gmail.com>
2022-01-17 18:21:11 +0300

    Initial

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="front">
+        <figure>
+          <img src="" alt="" />
+        </figure>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+      </div>
+      <div className="back">arka</div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+import Form from './Form';
+
+function Container() {
+  return (
+    <div className="container">
+      <Card />
+      <Form />
+    </div>
+  );
+}
+
+export default Container;
